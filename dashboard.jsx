b5aca26a8f376516876b166510304f93a26cf418
Jyotsna Thakur <j.thakur2608@gmail.com>
2025-04-20 22:56:56 +0530

    Create dashboard.jsx

---
+++ dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import RecommendationList from "./RecommendationList";
+
+const Dashboard = () => {
+  return (
+    <div>
+      <h1>Welcome to Personalized Learning</h1>
+      <RecommendationList />
+    </div>
+  );
+};
+
+export default Dashboard;
